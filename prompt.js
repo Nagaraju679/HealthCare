ab19d1158b8a7d79c1a49f74adba662b2a265dfb
Nagaraju679 <nagarajuenumula679@gmail.com>
2025-03-04 11:57:29 +0530

    New Javascript Project

---
+++ prompt.js
@@ -0,0 +1,13 @@
+let userName = prompt("Enter Your Name?");
+let title = `Hello ${userName} Welcome To Our WebPage`;
+let height = prompt("Enter Your Height in cm");
+let weight = prompt("Enter Your Weight in kg");
+document.getElementById("h").innerHTML = title;
+document.getElementById("p1").innerHTML = `Your Height Is : ${height} cm`;
+document.getElementById("p2").innerHTML = `Your Weight Is : ${weight} kg`;
+if(weight>70) {
+    document.getElementById("para1").innerHTML = "YOUR WEIGHT IS TOO HEAVY TRY TO DECREASE";
+}
+else {
+    document.getElementById("para2").innerHTML = "DONT WORRY YOUR HEALTH IS NORMAL"
+}
