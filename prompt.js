2c170c872ca1969b562b4abd57548b0cb6596017
Nagaraju679 <nagarajuenumula679@gmail.com>
2025-05-15 14:06:30 +0530

    Update prompt.js

--- prompt.js
+++ prompt.js
@@ -5,9 +5,13 @@ let weight = Number(prompt("Enter Your Weight in kg"));
 document.getElementById("h").innerHTML = title;
 document.getElementById("p1").innerHTML = `Your Height Is : ${height} cm`;
 document.getElementById("p2").innerHTML = `Your Weight Is : ${weight} kg`;
-if(weight>70) {
-    document.getElementById("para1").innerHTML = "YOUR WEIGHT IS TOO HEAVY TRY TO DECREASE";
-}
+if(weight===1)
+    if(weight>70) {
+        document.getElementById("para1").innerHTML = "YOUR WEIGHT IS TOO HEAVY TRY TO DECREASE";
+    }
+    else {
+        document.getElementById("para2").innerHTML = "DONT WORRY YOUR HEALTH IS NORMAL"
+    }
 else {
-    document.getElementById("para2").innerHTML = "DONT WORRY YOUR HEALTH IS NORMAL"
+    alert("Enter Valid Input");
 }
